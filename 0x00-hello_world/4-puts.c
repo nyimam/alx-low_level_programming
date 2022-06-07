171df483a934b93762b453bb02b245414611a443
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-07 11:51:10 -0700

    Create c programme that puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
-/* Main function to print our string*/
-
+/*
+ * Main block - function to print our string
+ * Return: 0
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
