a9eb7c9cc8e8752014888b52ef7c705a5721d1ff
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-08 14:10:43 -0700

    Print without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints string
+ * Return: 1
+ */
+int main(void)
+{
+char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(2, ch, sizeof(ch));
+return (1);
+}
