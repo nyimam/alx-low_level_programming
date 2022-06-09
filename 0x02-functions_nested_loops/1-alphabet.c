7c8ba92357d592ed817bb1fdd6bd85b6f070e968
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-09 06:04:02 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ * Returns: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <='z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
