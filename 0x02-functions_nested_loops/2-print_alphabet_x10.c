011ce8520b63b69db24a4387021c7b1974b1d038
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-09 06:36:22 -0700

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char letter = 'a';
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
 		_putchar('\n');
 	}
 }
