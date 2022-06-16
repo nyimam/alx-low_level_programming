f464f83ae8fe369b46527e483f51800d1441938c
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-16 02:52:33 -0700

    Reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array
+ * @a: pointer
+ * @n: array length
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, half;
+
+	for (half = n / 2; half > 0; half --, i++)
+	{
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
+	}
+}
