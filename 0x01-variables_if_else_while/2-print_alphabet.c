9516626275364c0ef280e7a96c636104fcc859ef
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-17 04:55:17 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print each letter of the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter < 'z'; letter++)
+	{
+		putchar(letter);
+		putchar('\n');
+	}
+	return (0);
+}
