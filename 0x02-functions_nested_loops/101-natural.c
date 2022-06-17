9c046790045e812ac9b37a4627dc42087938aaf3
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-16 21:58:36 -0700

    Print multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - computes ant prints sum of multiples
+ * Return: 0
+ */
+int main(void)
+{
+	int c = sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+			sum += c;
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
