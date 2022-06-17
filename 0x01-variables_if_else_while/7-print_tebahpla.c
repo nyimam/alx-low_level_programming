cc3e52ea5d89d350fdfc8737d7a15f2b3458a3aa
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-17 05:45:21 -0700

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
