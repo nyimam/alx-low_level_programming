b2c951ded8ae1d5510191e6bb05b35aba0e19b1f
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-16 22:36:31 -0700

    Print 98 Fibbonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,14 +9,14 @@ int main(void)
 	int i;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 96; i++)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 97)
+		if (i < 95)
 			printf(", ");
 	}
 	putchar('\n');
