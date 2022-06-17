55e8ec237999cac9c0a7ca1ebbf7fc486660d079
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-17 05:12:59 -0700

    Print alphabet without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,11 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z' && (letter != ('q' || 'e'); letter++)
-		putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' || letter != 'q')
+			putchar(letter);
+	}
 	putchar('\n');
 	return (0);
 }
