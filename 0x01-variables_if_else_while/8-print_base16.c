f1280bc0ed06088f43a1824f47fb988b01e42c6e
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-17 06:15:30 -0700

    Print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 		putchar('0' + n);
-	for (letter = 'a'; letter < g; letter++)
+	for (letter = 'a'; letter < 'g'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);
