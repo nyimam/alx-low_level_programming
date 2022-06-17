724adc503097847faf268547c218c85f35def4e2
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-17 06:14:39 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char letter;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+	for (letter = 'a'; letter < g; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
