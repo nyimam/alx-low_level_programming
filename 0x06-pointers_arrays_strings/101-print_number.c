986e0a41cea5bf88ba9a392087be26dc85546fe6
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-18 22:41:23 -0700

    Printing a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,5 +7,18 @@
 
 void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
