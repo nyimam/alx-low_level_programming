2d4b8730f3946bd676318aadbdae2336e60b4c7f
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-26 06:48:10 -0700

    Create 5-more_prints and update README.md amnd main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 0 to 14,
+ * followed by new line 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, a;
+
+	for (a = 0; a < 11, a++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
