d6b1b9cecc0bc928e74c94d2b7b28a9f84a68d0b
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-28 09:34:12 -0700

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,16 @@ int main(void)
 		a = i + 1;
 
 		if (a % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (a % 5 == 0)
-		{
-			if (a == 100)
-				printf("Buzz");
-			else
-				printf("Buzz ");
-		}
+			printf("Buzz");
 		else if (a % 3 == 0 && a % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
-			printf("%i ", a);
+			printf("%i", a);
+
+		if (a != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
