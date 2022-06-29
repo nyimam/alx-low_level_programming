17d5d731505dd299ac565611ba7387ed3bc08025
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-06-28 22:37:35 -0700

    Add 101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - prints an int
+ * @n: number to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -n;
+	}
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
