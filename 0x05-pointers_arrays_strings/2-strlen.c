0a872a847ff8b09262a15dbed13ae6282c5be7bd
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-07-01 04:42:07 -0700

    Add 2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio>
+/**
+ * _strlen - prints out the length of the string
+ * @s: pointer to the string (array of chars)
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int a = sizeof(*s);
+
+	return (a / 4);
+}
