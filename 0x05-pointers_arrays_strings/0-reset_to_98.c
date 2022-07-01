28e58fb454e0a70370ced6f3264c1e66be3ce974
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-07-01 02:56:53 -0700

    Add directory, main, and 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value that the pointer points to
+ * @n: pointer to an int value
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+<h1> Pointers, arrays and strings </h1>
