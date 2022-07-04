eab26f457db1c9fe4cc5063be13cb5712cb70842
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-07-04 06:49:20 -0700

    Add print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,15 @@ void print_array(int *a, int n)
 {
 	int length;
 
-	for (length = 0; n >= 0 && length < (n - 1); length++)
-		printf("%i, ", a[length]);
+	if (n < 0)
+		printf("\n");
+	else if (n == 0)
+		printf("%i\n", a[0]);
+	else if (n > 0)
+	{
+		for (length = 0; length < (n - 1); length++)
+			printf("%i, ", a[length]);
 
-	printf("%i\n", a[n - 1]);
+		printf("%i\n", a[n - 1]);
+	}
 }
