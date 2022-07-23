cc72559d29b356510fc482c275a739823535b55b
Wamunyima Muya <wamunyimamuya@gmail.com>
2022-07-23 10:17:07 -0700

    Add wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,3 +11,6 @@ int check_prime(int n, int resp);
 int is_palindrome(char *s);
 int check(char *s, int start, int end, int pair);
 int last_index(char *s);
+int wildcmp(char *s1, char *s2);
+int checkLast(char *s, int i);
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
